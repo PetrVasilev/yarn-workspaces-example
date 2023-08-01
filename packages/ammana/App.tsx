7ca28286b8b05b984f0bde7fd579323d50ac4dc1
Petr Vasilev <vpa17@ya.ru>
2023-08-01 14:49:55 +0600

    init

---
+++ packages/ammana/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import {CommonApp} from '@fingular/app';
+
+export function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello from Ammana</Text>
+      <CommonApp />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ packages/app/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const CommonApp = () => {
+  return (
+    <View>
+      <Text>Hello from Common App</Text>
+    </View>
+  );
+};
